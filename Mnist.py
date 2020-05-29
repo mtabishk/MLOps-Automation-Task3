75aa7fd20028ba8a977c0a384c625e332b7cd6c3
Muhammad Tabish <mtabishkhanday@gmail.com>
2020-05-30 00:05:18 +0530

    v3

--- Mnist.py
+++ Mnist.py
@@ -1,86 +1,59 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-from __future__ import print_function
-import keras
-from keras.datasets import mnist
+# keras imports for the dataset and building our neural network
+from keras.datasets import fashion_mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-import os
+from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
+from keras.utils import np_utils
+from keras.callbacks import Callback
+# to calculate accuracy
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-def model_train(epoch,n):
-    batch_size = 32
-    num_classes = 10
-    epochs = epoch
-
-    # input image dimensions
-    img_rows, img_cols = 28, 28
-
-    # the data, split between train and test sets
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    if K.image_data_format() == 'channels_first':
-        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-        input_shape = (1, img_rows, img_cols)
-    else:
-        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
-
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
-
-    # convert class vectors to binary class matrices
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-
-    model = Sequential()
-    
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=input_shape))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    if n>1:
-        model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))
-
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.Adadelta(),
-                  metrics=['accuracy'])
-
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              verbose=1,
-              validation_data=(x_test, y_test))
-    score = model.evaluate(x_test, y_test, verbose=0)
-    a=score[1]*100
-    model.save("MNIST.h5")
-    os.system("mv /MNIST.h5 /mycode")
-    return a
-
-no_epoch=1
-no_layer=1
-accuracy_train_model=model_train(no_epoch,no_layer)
-f = open("accuracy.txt","w+")
-f.write(str(accuracy_train_model))
-f.close()
-os.system("mv /accuracy.txt /mycode")
+# class for writing the accuracy in a file
+class myCallback(Callback):
+      def on_epoch_end(self, epoch, logs={}):
+          
+            file='/root/workspace/mlops/output.txt' 
+            var=logs.get('accuracy')
+            with open(file, 'w') as filetowrite:
+                filetowrite.write(np.array2string(var))
 
 
-# In[ ]:
+
+
+callbacks = myCallback()
+# loading the dataset
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+# building the input vector from the 28x28 pixels
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+# normalizing the data to help with the training
+X_train /= 255
+X_test /= 255
+
+# one-hot encoding using keras' numpy-related utilities
+n_classes = 10
+print("Shape before one-hot encoding: ", y_train.shape)
+Y_train = np_utils.to_categorical(y_train, n_classes)
+Y_test = np_utils.to_categorical(y_test, n_classes)
+print("Shape after one-hot encoding: ", Y_train.shape)
+
+# building a linear stack of layers with the sequential model
+model = Sequential()
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+#model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+# compiling the sequential model
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+#hyperparameters
+epoch=5
+# training the model for 5 epochs
+history = model.fit(
+            X_train,Y_train, epochs=epoch,callbacks=[callbacks]
+    )
